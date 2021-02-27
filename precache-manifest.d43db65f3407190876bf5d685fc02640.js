a078a58e7b8d979ab13dee982c8bd359a40acc13
qqq <git@aaa.jp>
2021-02-27 15:07:22 +0900

    add func change Event

---
+++ precache-manifest.d43db65f3407190876bf5d685fc02640.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8290eeb81a8116684a07",
+    "url": "/CalcEventNumOfTimes/css/app.9fd904f9.css"
+  },
+  {
+    "revision": "dcabcef0d96f2326f1c8",
+    "url": "/CalcEventNumOfTimes/css/chunk-vendors.c57a1899.css"
+  },
+  {
+    "revision": "dfca7d721db4e7247a84c0f9160e4412",
+    "url": "/CalcEventNumOfTimes/img/itemicon_108203.dfca7d72.png"
+  },
+  {
+    "revision": "112518f7c70bdbccbab016ba8cd5ad4d",
+    "url": "/CalcEventNumOfTimes/index.html"
+  },
+  {
+    "revision": "8290eeb81a8116684a07",
+    "url": "/CalcEventNumOfTimes/js/app.f522bf3f.js"
+  },
+  {
+    "revision": "dcabcef0d96f2326f1c8",
+    "url": "/CalcEventNumOfTimes/js/chunk-vendors.1c855e23.js"
+  },
+  {
+    "revision": "1e023f9790456db2b85ac1ac38c1b91f",
+    "url": "/CalcEventNumOfTimes/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/CalcEventNumOfTimes/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/CalcEventNumOfTimes/precache-manifest.d43db65f3407190876bf5d685fc02640.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "kirafan_event"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
